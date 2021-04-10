d3262b877612c777a4393b39638d7b707513bb33
Popovycha <artem220397@gmail.com>
2021-04-10 17:03:14 -0400

    Add files via upload

---
+++ Chapter 25/src/EvenNumbers/EvenNumberHolder.java
@@ -0,0 +1,13 @@
+package EvenNumbers;
+
+public class EvenNumberHolder {
+
+    private int currentEven = 0;
+
+    public int getNextEven() {
+            synchronized (this) {
+            currentEven = currentEven + 2;
+        }
+        return currentEven;
+    }
+}
